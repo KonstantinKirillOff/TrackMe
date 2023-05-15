8b2294c5a992798428d8d046a0a45f743f458d02
Konstantin Kirillov <kirilloffkostya@gmail.com>
2023-05-15 10:42:52 +0300

    add pageVC for onboarding

--- TrackMe/App/AppDelegate.swift
+++ TrackMe/App/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		
 		window = UIWindow(frame: UIScreen.main.bounds)
 		window?.makeKeyAndVisible()
-		window?.rootViewController = TabBarViewController()
+		window?.rootViewController = OnboardingViewController(transitionStyle: .pageCurl, navigationOrientation: .horizontal)
 		return true
 	}
 	
---
+++ TrackMe/ViewControllers/OnboardingPageVC/OnboardingViewController.swift
@@ -0,0 +1,112 @@
+//
+//  OnboardingViewController.swift
+//  TrackMe
+//
+//  Created by Konstantin Kirillov on 15.05.2023.
+//
+
+import UIKit
+
+final class OnboardingViewController: UIPageViewController {
+	lazy var pages: [UIViewController] = {
+		let screenOne = OnboardingScreenOneViewController()
+		let screenTwo = OnboardingScreenTwoViewController()
+		return [screenOne, screenTwo]
+	}()
+	
+	lazy var pageControl: UIPageControl = {
+		let pageControl = UIPageControl()
+		pageControl.numberOfPages = pages.count
+		pageControl.currentPage = 0
+		
+		pageControl.currentPageIndicatorTintColor = UIColor.ypBlack
+		pageControl.pageIndicatorTintColor = UIColor.ypGray
+		
+		pageControl.translatesAutoresizingMaskIntoConstraints = false
+		return pageControl
+	}()
+	
+	lazy var onboardingFinishButton: UIButton = {
+		let button = UIButton()
+		button.setTitle("Вот это технологии!", for: .normal)
+		button.backgroundColor = UIColor.ypBlack
+		button.setTitleColor(UIColor.ypWhite, for: .normal)
+		button.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+		button.layer.cornerRadius = 16
+		button.addTarget(self, action: #selector(finishButtonTapped), for: .touchUpInside)
+		button.translatesAutoresizingMaskIntoConstraints = false
+		return button
+	}()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		dataSource = self
+		delegate = self
+		if let firstScreen = pages.first {
+			setViewControllers([firstScreen], direction: .forward, animated: true)
+		}
+		setupUIElements()
+    }
+	
+	private func setupUIElements() {
+		view.addSubview(onboardingFinishButton)
+		NSLayoutConstraint.activate([
+			onboardingFinishButton.heightAnchor.constraint(equalToConstant: 60),
+			onboardingFinishButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+			onboardingFinishButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+			onboardingFinishButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50)
+		])
+		
+		view.addSubview(pageControl)
+		NSLayoutConstraint.activate([
+			pageControl.topAnchor.constraint(equalTo: onboardingFinishButton.topAnchor, constant: -24),
+			pageControl.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+		])
+	}
+	
+	@objc
+	private func finishButtonTapped() {
+		let tabBarVC = TabBarViewController()
+		tabBarVC.modalPresentationStyle = .fullScreen
+		present(tabBarVC, animated: true)
+	}
+}
+
+extension OnboardingViewController: UIPageViewControllerDataSource {
+	func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+		guard let viewControllerIndex = pages.firstIndex(of: viewController) else {
+			return nil
+		}
+		
+		let prevIndex = viewControllerIndex - 1
+		
+		guard prevIndex >= 0 else {
+			return nil
+		}
+		
+		return pages[prevIndex]
+	}
+	
+	func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+		guard let viewControllerIndex = pages.firstIndex(of: viewController) else {
+			return nil
+		}
+		
+		let nextIndex = viewControllerIndex + 1
+		
+		guard nextIndex < pages.count else {
+			return nil
+		}
+		
+		return pages[nextIndex]
+	}
+}
+
+extension OnboardingViewController: UIPageViewControllerDelegate {
+	func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+		if let currentVC = pageViewController.viewControllers?.first,
+		   let currentIndex = pages.firstIndex(of: currentVC) {
+			pageControl.currentPage = currentIndex
+		}
+	}
+}
